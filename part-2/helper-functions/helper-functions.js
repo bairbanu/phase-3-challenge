f1a4d3ac71441f0a708e56dfa72accb660fbde17
zubairnahmed <zubair.ahmed@alumni.stanford.edu>
2017-08-08 18:01:25 -0700

    added commented node-print code to helper functions

--- part-2/helper-functions/helper-functions.js
+++ part-2/helper-functions/helper-functions.js
@@ -76,3 +76,48 @@ function errorHandler(err) {
 }
 
 module.exports = { listProducts, listOrders, listRealShoppers, errorHandler };
+
+// code with node-print library looks like:
+
+/*
+const print = require('node-print');
+
+function listProducts(list) {
+  const processedList = [];
+
+  list.forEach((item) => {
+    processedList.push({ "Product Name": item.name, "Section": process.argv[3]});
+  })
+
+  print.pt(processedList);
+  process.exit();
+}
+
+function listOrders(list) {
+  const processedList = [];
+
+  list.forEach((item) => {
+    processedList.push({"Order ID": item.id, "Total Cost": item.sum});
+  })
+
+  print.pt(processedList);
+  process.exit();
+}
+
+function listRealShoppers(list) {
+  const processedList = [];
+
+  list.forEach((item) => {
+    processedList.push({"Shopper Name": item.name, "Number of Orders": item.count});
+  })
+
+  print.pt(processedList);
+  process.exit();
+}
+
+function errorHandler(err) {
+  console.log('An error occured', err);
+}
+
+module.exports = { listProducts, listOrders, listRealShoppers, errorHandler };
+*/
